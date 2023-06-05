4580a97398409c8b9b44df5714819efae98c42e4
JCaeta <63880187+JCaeta@users.noreply.github.com>
2023-06-05 18:32:12 -0300

    Deploying configuration updated

--- api.py
+++ api.py
@@ -38,9 +38,9 @@ def analysis():
 def available():
     return jsonify(True)
 
-
 if __name__ == '__main__':
-    # app.run(debug=True)
-    # app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
-
+    # app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(
+    host="0.0.0.0",
+    port=5000
+)
